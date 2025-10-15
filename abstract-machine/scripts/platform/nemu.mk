a5a55aae98241b91f2dd36631bb615ad2397d4c8
mingya <jin2899042558@163.com>
2025-10-15 23:18:28 +0800

    BATCH need to be set manually

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -13,7 +13,9 @@ LDSCRIPTS += $(AM_HOME)/scripts/linker.ld
 LDFLAGS   += --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
+ifdef BATCH
 NEMUFLAGS += -b			#AM自动进入batch modes
+endif
 
 MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = the_insert-arg_rule_in_Makefile_will_insert_mainargs_here
