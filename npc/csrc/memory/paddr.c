36000031ccbd78976329ebec8c39444d4c4030de
mingya <jin2899042558@163.com>
2025-11-16 11:21:24 +0800

    reconstruct memory

--- npc/csrc/memory/paddr.c
+++ npc/csrc/memory/paddr.c
@@ -1,33 +1,21 @@
-#include <stdio.h>
+#include "host.h"
 #include "memory/paddr.h"
+#include "isa.h"
 
 static uint8_t pmem[MSIZE] __attribute((aligned(4096)))= {};
 
-static const uint32_t img [] = {
-  0x00000297,  // auipc t0,0
-  0x00028823,  // sb  zero,16(t0)
-  0x0102c503,  // lbu a0,16(t0)
-  0x00100073,  // ebreak (used as nemu_trap)
-  0xdeadbeef,  // some data
-};
-
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + MBASE; }
-
-static word_t host_read(void *addr, int len) {
-  switch (len) {
-    case 1: return *(uint8_t  *)addr;
-    case 2: return *(uint16_t *)addr;
-    case 4: return *(uint32_t *)addr;
-    default: assert(0);         //不支持的读取长度
-  }
-}
 
 word_t pmem_read(paddr_t addr, int len){
     word_t ret = host_read(guest_to_host(addr), len);
     return ret;
 }
 
+static void pmem_write(paddr_t addr, int len, word_t data) {
+    host_write(guest_to_host(addr), len, data);
+}
+
 void init_mem(){
-    memcpy(guest_to_host(RESET_VECTOR), img, sizeof(img));
+    
 }
--- npc/include/memory/paddr.h
+++ npc/include/memory/paddr.h
@@ -13,9 +13,12 @@
 #define PMEM_RIGHT ((paddr_t)MBASE + MSIZE - 1)
 #define RESET_VECTOR (PMEM_LEFT + PC_RESET_OFFSET)
 
-void init_mem();
 uint8_t* guest_to_host(paddr_t paddr);
 paddr_t host_to_guest(uint8_t *haddr);
+
+static inline bool in_pmem(paddr_t addr) {
+    return addr - MBASE < MSIZE;
+}
 
 word_t pmem_read(paddr_t addr, int len);
 void pmem_write(paddr_t addr, int len, word_t data);
