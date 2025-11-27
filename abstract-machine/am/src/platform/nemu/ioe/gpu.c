98bfb21e03090be45e183aec3fe58cc11be2a77c
mingya <jin2899042558@163.com>
2025-11-27 20:45:50 +0800

    [ADD] AM gpc

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,17 +4,32 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  int w = io_read(AM_GPU_CONFIG).width;
+  int h = io_read(AM_GPU_CONFIG).height;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = 0;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = (inl(VGACTL_ADDR) >> 16),
+    .height = (inl(VGACTL_ADDR) & 0xFFFF),
+    .vmemsz = (inl(VGACTL_ADDR) >> 16) * (inl(VGACTL_ADDR) & 0xFFFF)
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int i, j;
+  uint32_t width = io_read(AM_GPU_CONFIG).width;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (j = 0; j < ctl->h; j ++) {
+    for (i = 0; i < ctl->w; i ++) {
+      fb[(ctl->x + i) + (ctl->y + j) * width] = ((uint32_t*)ctl->pixels)[i + j * ctl->w];
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
