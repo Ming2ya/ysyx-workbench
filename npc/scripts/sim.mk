f9e8fc4d4f3f068126002b4812593e0e2ba75ff8
mingya <jin2899042558@163.com>
2025-12-01 11:05:47 +0800

    [MOD] make structure

---
+++ npc/scripts/sim.mk
@@ -0,0 +1,15 @@
+IMG ?=
+NPC_EXEC = $(BINARY) $(IMG)
+
+$(VSRCS): verilog
+
+$(BINARY): $(VSRCS) $(CSRCS) $(OBJS)
+	$(call git_commit, "sim RTL") # DO NOT REMOVE THIS LINE!!!
+	verilator $(VFLAGS) $(addprefix -CFLAGS , $(CFLAGS)) $^ \
+		 --Mdir $(OBJ_DIR) -o $@
+
+sim: $(BINARY)
+	$(NPC_EXEC)
+
+gdb: $(BINARY)
+	gdb -s $(BINARY) -args $(NPC_EXEC)
