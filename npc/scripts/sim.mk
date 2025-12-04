9f0ac52e85519bafbd7d379d10160f026fbd56a4
mingya <jin2899042558@163.com>
2025-12-05 00:08:11 +0800

    [MOD] build C srouce natively(to avoid same name conflict), C, CC conflict

--- npc/scripts/sim.mk
+++ npc/scripts/sim.mk
@@ -1,12 +1,25 @@
 IMG ?=
 NPC_EXEC = $(BINARY) $(IMG)
 
+INCLUDES = $(addprefix -I, $(INC_PATH))
+CFLAGS  := -O2 -MMD  $(INCLUDES) $(CFLAGS) #-Wall -Werror
+LDFLAGS := -O2 $(LDFLAGS)
+
+OBJS = $(CSRCS:%.c=$(OBJ_DIR)/%.o)
+
+# Compilation patterns
+$(OBJ_DIR)/%.o: %.c
+	@echo + CC $<
+	@mkdir -p $(dir $@)
+	@$(CC) $(CFLAGS) -c -o $@ $<
+	$(call call_fixdep, $(@:.o=.d), $@)
+
 $(VSRCS): verilog
 
-$(BINARY): $(VSRCS) $(CSRCS) $(OBJS)
+$(BINARY): $(VSRCS) $(CXXSRCS) $(OBJS)
 	$(call git_commit, "sim RTL") # DO NOT REMOVE THIS LINE!!!
-	verilator $(VFLAGS) $(addprefix -CFLAGS , $(CFLAGS)) $^ \
-		 --Mdir $(OBJ_DIR) -o $@
+	verilator --Mdir $(OBJ_DIR) -o $@ $^ $(VFLAGS) \
+			$(addprefix -CFLAGS , $(CFLAGS)) $(addprefix -LDFLAGS , $(LDFLAGS))
 
 sim: $(BINARY)
 	$(NPC_EXEC)
